f8893688e93d47b03be186a3ff6b7bc890ff8827
Fikry C. Farenza <vickyfarenza@gmail.com>
2022-11-11 08:40:09 +0700

    Fixed register sql :hammer:

--- register-check.php
+++ register-check.php
@@ -6,7 +6,7 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $password = $_POST["password"];
 $phone = $_POST["phone"];
-$sql = "INSERT INTO user(email, password, name, phone) VALUES ('".$name."', '".$email."', '".$password."','".$phone."')";
+$sql = "INSERT INTO user(name, email, password, phone) VALUES ('".$name."', '".$email."', '".$password."','".$phone."')";
 
 
 if ($conn->query($sql)) {
