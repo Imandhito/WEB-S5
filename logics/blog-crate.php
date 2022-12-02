e1706cd133b3c4d0be488c92ed6967cdd3865bf1
Munyalisme <57283675+Munyalisme@users.noreply.github.com>
2022-12-02 07:28:52 +0700

    Blog
    blog.php & blog-crate.php

---
+++ logics/blog-crate.php
@@ -0,0 +1,16 @@
+<?php$name = $email = $hobby = $address = $gender = $password = "";
+
+$title = $_POST["Title"];
+$text = $_POST["Article Text"];
+$img_url = $_POST["Image Link"];
+$sql = "INSERT INTO blog(title, text, role, img_url) VALUES ('" . $$title . "', '" . $text . "', 'user', '" . $img_url . "')";
+
+
+if ($conn->query($sql)) {
+    header('location: ../login.php');
+} else {
+    echo "Error :" . $conn->error;
+}
+
+
+?>
