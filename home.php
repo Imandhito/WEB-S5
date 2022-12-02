e3f95a186fc87f4d0c7d8b87e69d11072e89831b
KenjiPrayogo <alfonso.prayogo@gmail.com>
2022-12-02 11:21:42 +0700

    Auto stash before rebase of "origin/master"

--- home.php
+++ home.php
@@ -34,11 +34,11 @@
   <header id=" header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      
       <a href="home.php" class="logo d-flex align-items-center">
         <span class="d-none d-lg-block">RoamRent.</span>
       </a>
-      </a>
+     
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
