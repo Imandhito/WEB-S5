7e000684afbfe3d2055258d309909ff9928f78a5
Fikry C. Farenza <vickyfarenza@gmail.com>
2022-12-15 05:27:45 +0700

    rename item list :hammer:

--- vehicle.php
+++ vehicle.php
@@ -16,33 +16,9 @@ if (!isset($_SESSION['user_id'])) {
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/nice-style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: NiceAdmin - v2.4.0
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-  <?php include 'logics/connect.php';
+  <?php
+  include("layout-head-import-nice.php");
+  include 'logics/connect.php';
   $sql = 'SELECT * FROM vehicle';
   $result = $conn->query($sql);
   ?>
@@ -284,33 +260,17 @@ if (!isset($_SESSION['user_id'])) {
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="home.php">
+        <a class="nav-link collapsed" href="home.php">
           <i class="bi bi-grid"></i>
           <span>Home</span>
         </a>
       </li><!-- End Home Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Vehicles</span><i class="bi bi-chevron-down ms-auto"></i>
+        <a class="nav-link " href="vehicle.php">
+          <i class="bi bi-menu-button-wide"></i>
+          <span>Vehicles</span>
         </a>
-        <ul id="components-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="item_list.php">
-              <i class="bi bi-circle"></i><span>Car</span>
-            </a>
-          </li>
-          <li>
-            <a href="item_list.php">
-              <i class="bi bi-circle"></i><span>Motorcycle</span>
-            </a>
-          </li>
-          <li>
-            <a href="item_list.php">
-              <i class="bi bi-circle"></i><span>Bicycle</span>
-            </a>
-          </li>
-        </ul>
       </li><!-- End Vehicles Nav -->
 
       <li class="nav-item">
