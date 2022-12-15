b7c2252a9312be89c891aad8834e7b4f267aab55
Munyalism <simandhitto@gmail.com>
2022-12-16 06:58:08 +0700

    -_-

--- blog.php
+++ blog.php
@@ -230,12 +230,10 @@
               <div class="sidebar-item categories">
                 <h3 class="sidebar-title">Categories</h3>
                 <ul class="mt-3">
-                  <li><a href="#">General <span>(25)</span></a></li>
-                  <li><a href="#">Lifestyle <span>(12)</span></a></li>
-                  <li><a href="#">Travel <span>(5)</span></a></li>
-                  <li><a href="#">Design <span>(22)</span></a></li>
-                  <li><a href="#">Creative <span>(8)</span></a></li>
-                  <li><a href="#">Educaion <span>(14)</span></a></li>
+                  <li><a href="blog-tag.php?id=1">News</a></li>
+                  <li><a href="blog-tag.php?id=2">Travel</a></li>
+                  <li><a href="blog-tag.php?id=3">Life Hacks</a></li>
+                  <li><a href="blog-tag.php?id=4">Promo</a></li>
                 </ul>
               </div><!-- End sidebar categories-->
 
